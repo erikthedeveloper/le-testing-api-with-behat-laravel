666fa6516f94b50eaa70f6a4fab7ca4f97517f28
Erik Aybar <erikthedeveloper@gmail.com>
2014-12-04 07:23:38 -0700

    Updates composer behat, web-api-extension, ide-helper (+ app.php)

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+
 	),
 
 	/*
